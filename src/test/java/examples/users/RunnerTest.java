31cbb25de8b4f6e4404cbd08833be8b84398faef
Danieljimenez527 <Danycangri05@gmail.com>
2025-05-01 17:56:51 -0500

    pruebas api completadas

--- src/test/java/examples/users/RunnerTest.java
+++ src/test/java/examples/users/RunnerTest.java
@@ -7,7 +7,7 @@ class RunnerTest {
     @Karate.Test
     Karate testUsers() {
         return Karate.run("busquedacandidato", "creacioncandidato",
-                "role.feature", "actualizarcandidato.feature", "candidatestate.feature", "procesos.feature",
+                "role.feature", "actualizarcandidato.feature", "candidatestate.feature", "procesoscandidatos.feature",
                 "postulacioncandidato.feature").relativeTo(getClass());
     }
 
